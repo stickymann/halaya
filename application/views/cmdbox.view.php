c06c32f190ab9000959ea6796d14e965035a2235
Dunstan Nesbit _on_funkybee <dunstan.nesbit@gmail.com>
2014-03-08 04:37:18 -0400

    Cmdbox notification css added

--- application/views/cmdbox.view.php
+++ application/views/cmdbox.view.php
@@ -16,5 +16,25 @@
 		<tr><td><b>Environment :</b></td><td align="left"><?php print $environment; ?></td></tr>
 	</table>
 </div>
+<style>
+#notify-container { border: #000 solid 1px; background: #ebf2f9; margin: 1px 0px 0px 0px; padding: 5px 10px 20px 10px; }
+#notify-label { 
+	border: green solid 0px;  font-family: verdana, arial, helvetica, sans-serif; font-size: 1em; 
+	color: #0000ad; font-size: 1em; font-weight: bold; text-align: left;
+	width: 120px; float: left;
+}
+#notify-circle {
+    border: green solid 0px; background: green;
+    width: 30px; height: 30px;
+    border-radius:50%;
+    color: #fff; font-size: 2.5em; font-weight: normal; text-align: center;
+    font-family: verdana; line-height: 1.2em;
+	float: left;
+}
+</style>
+<div id="notify-container">&nbsp
+<div id="notify-label">Order Entry<br>Uploads Pending</div>
+<div id="notify-circle">0</div>
+</div>
 </body>
 </html>   
